767e09d2e7f3266621678755e6f9f28246fa06fc
Obeida Arafa <obeidaarafa@gmail.com>
2025-12-29 01:28:19 +0300

    feat: add pagination, filtering, and sorting for products

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,8 +3,37 @@ const AppError = require("../utils/AppError");
 const asyncHandler = require("../middlewares/asyncHandler");
 
 const getAllProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find();
-  res.status(200).json(products);
+  const query = req.query;
+  // pagination
+  const limit = Number(query.limit) || 5;
+  const page = Number(query.page) || 1;
+  const skip = (page - 1) * limit;
+
+  // sorting
+  let sortBy = "-createdAt";
+  if (req.query.sort) {
+    sortBy = req.query.sort.split(",").join(" ");
+  }
+
+  // filtering
+  const queryObj = { ...req.query };
+  const excludedFields = ["page", "sort", "limit"];
+  excludedFields.forEach((el) => delete queryObj[el]);
+
+  let queryStr = JSON.stringify(queryObj);
+  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+  const filter = JSON.parse(queryStr);
+
+  const products = await Product.find(filter)
+    .sort(sortBy)
+    .skip(skip)
+    .limit(limit);
+
+  res.status(200).json({
+    success: true,
+    data: products,
+  });
 });
 
 const getProduct = asyncHandler(async (req, res) => {
